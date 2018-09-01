7b228f10d72ebe034734f3022241efa174de4846
Daniel Hernandez <dh2832@columbia.edu>
2018-08-31 20:45:12 -0400

    Add bunch of comments

--- neurolib/models/aec.py
+++ neurolib/models/aec.py
@@ -30,16 +30,41 @@ class BasicAE(Model):
   """
   def __init__(self, ydim, xdim, specs):
     """
+    PHILOSOPHY: Classes that inherit from Model are those that will be seen by
+    the user. For the moment, I have decided that the tensorflow graph is
+    created upon initialization. Hence, this classes should implement at least
+    the following methods:
+    
+    train(...)
+    sample(...)
+    
+    Moreover, as the project grows, other abstract subclasses will likely appear
+    between the Model class and concrete classes such as this one that give rise
+    to a more hierarchical structure of statistical models.
+    
+    
     TODO: Need to worry about dealing with separate models cocnurrently which
     requires handling multiple active graphs
     """
-    # TODO: Decide how to pass ydim
+    # TODO: Decide how to pass these things
+    
+    # TODO: The idea here is that there are some hyperparameters that the user
+    # MUST provide. Those should not be in the dictionary of specs, rather the
+    # user should be forced to provide them on initialization.
     self.ydim = ydim
     self.xdim = xdim
     super(BasicAE, self).__init__()
     self.specs = specs
     
+    # The main scope for this model. 
+    
+    # TODO: There is a design problem here if say, we want to work with 2
+    # different models of this class. One possibility is to embed all this in a
+    # Singleton class that handles things such as providing the right scope for
+    # each model. This shouldn't be hard to implement.
     self.main_scope = 'BasicAE'
+    
+    # Build the model
     self._build()
     
   def _build(self):
@@ -49,7 +74,18 @@ class BasicAE(Model):
     specs = self.specs
     ydim = self.ydim
     b_size = specs['b_size'] if 'b_size' in self.specs else 1
+    
+    # These line builds the specs that will be used to construct the Encoders.
+    # Each Encoder object is an abstraction of the sequence:
+    #
+    # Code => Mapping => Code
     enc1_specs, enc2_specs =  self._build_encoder_specs()
+    
+    # TODO: Then there are the Trainer objects. This takes into account the
+    # possibility that we may want to train models in different ways. In the
+    # case of this simple AutoEncoder, the training procedure is just gradient
+    # descent on a cost function. This training strategy is implemented in
+    # GDTrainer which takes a cost and some specs.
     train_specs = {'lr' : 1e-6, 'optimizer' : 'adam'}
     
     enc_fac = EncoderFactory()
@@ -59,12 +95,18 @@ class BasicAE(Model):
       self.enc_H1 = enc_fac(self.Y, enc1_specs, out_name='H1')
       self.enc_Yprime = enc_fac(self.enc_H1, enc2_specs, out_name='Yprime')
       
-      self.cost = self.build_cost()
+      # TODO: Note the use of node_names below. Nodes correspond to the codes of
+      # the different Encoder objects and I am foreseeing to implement an
+      # automatic assignment of integers to the nodes as their names. The node
+      # names are used by the tensorflow session that is run during training to
+      # feed the proper tensors. Any idea to make this more elegant is
+      # appreciated.
+      self.cost = self._build_cost()
       self.trainer = GDTrainer(self.cost, train_specs,
                                node_names={0 : self.Y.name})
 #       self.output = AECBuilder.build(self.specs) 
     
-  def build_cost(self):
+  def _build_cost(self):
     """
     """
     Y = self.Y
